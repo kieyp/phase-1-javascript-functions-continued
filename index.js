9d5016f663c000a95f23f7fe3e8f89f2d64870e6
Boniface <Cheruiyotbony@gmail.com>
2023-10-08 00:37:17 +0300

    completed assingment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // code your solution here
+
+function saturdayFun(activity='roller-skate')
+{
+  return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity='go to the office')
+
+{
+
+  return`This Monday, I will ${activity}.`
+}
+
+(function(basenumber){
+
+  return basenumber +3 ;
+})
+
+
+function wrapAdjective(string='*')
+
+{
+  return function inner(parametre='special')
+  {
+    return `You are ${string}${parametre}${string}!`
+  }
+}
